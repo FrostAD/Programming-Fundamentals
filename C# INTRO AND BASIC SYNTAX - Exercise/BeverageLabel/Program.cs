7c0e3b30be46ca1c5a70358cb409961f7b43084e
Atanas Dimov <nasko312@gmail.com>
2018-02-04 22:23:39 +0200

    C# INTRO AND BASIC SYNTAX - Exercise

---
+++ C# INTRO AND BASIC SYNTAX - Exercise/BeverageLabel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeverageLabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energyContent = double.Parse(Console.ReadLine());
+            double sugarContent = double.Parse(Console.ReadLine());
+
+            energyContent = (volume / 100) * energyContent;
+            sugarContent = (volume / 100) * sugarContent;
+
+            Console.WriteLine($"{volume:f0}ml {name}:");
+            Console.WriteLine($"{energyContent}kcal, {sugarContent}g sugars");
+
+        }
+    }
+}
---
+++ C# INTRO AND BASIC SYNTAX - Exercise/DebitCardNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebitCardNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstPart = int.Parse(Console.ReadLine());
+            int secondPart = int.Parse(Console.ReadLine());
+            int thirdPart = int.Parse(Console.ReadLine());
+            int fourthPart = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstPart:d4} {secondPart:d4} {thirdPart:d4} {fourthPart:d4}");
+        }
+    }
+}

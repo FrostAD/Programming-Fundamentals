a69a8ac819931796f361da319f90b4fa8a4fcb73
Atanas Dimov <nasko312@gmail.com>
2018-02-05 23:27:41 +0200

    C# CONDITIONAL STATEMENTS AND LOOPS - Labs

---
+++ C# CONDITIONAL STATEMENTS AND LOOPS - Labs/BackInMinutes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackInMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes >= 60)
+            {
+                hours++;
+                minutes -= 60;
+            }
+
+            if (hours == 24)
+            {
+                hours -= 24;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
---
+++ C# CONDITIONAL STATEMENTS AND LOOPS - Labs/TheatrePromotions/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatrePromotions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+
+            if (age < 0 || age > 122)
+            {
+                Console.WriteLine("Error!");
+            }
+
+            if (typeOfDay == "Weekday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 12;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 18;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 12;
+                }
+            }
+            else if (typeOfDay == "Weekend")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 15;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 20;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 15;
+                }
+            }
+            else
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 5;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 12;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 10;
+                }
+            }
+
+            Console.WriteLine($"{price}$");
+        }
+    }
+}

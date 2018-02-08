c53e7589e956880c70dbe2b1c5966043c61cae3c
Atanas Dimov <nasko312@gmail.com>
2018-02-08 22:41:08 +0200

    C# CONDITIONAL STATEMENTS AND LOOPS - Exercise

---
+++ C# CONDITIONAL STATEMENTS AND LOOPS - Exercise/CakeIngredients/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = string.Empty;
+            int count = 0;
+            while (product != "Bake!")
+            {
+                product = Console.ReadLine();
+                if (product == "Bake!")
+                {
+                    break;
+                }
+                count++;
+                Console.WriteLine($"Adding ingredient {product}.");
+            }
+
+            Console.WriteLine($"Preparing cake with {count} ingredients.");
+        }
+    }
+}
---
+++ C# CONDITIONAL STATEMENTS AND LOOPS - Exercise/CaloriesCounter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloriesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int cheese = 500;
+            int tomato = 150;
+            int salami = 600;
+            int pepper = 50;
+            int total = 0;
+
+            for (int i = 1; i <= count; i++)
+            {
+                string input = Console.ReadLine().ToLower();
+                switch (input)
+                {
+                    case "cheese":
+                        total += 500;
+                        break;
+                    case "tomato sauce":
+                        total += 150;
+                        break;
+                    case "salami":
+                        total += 600;
+                        break;
+                    case "pepper":
+                        total += 50;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Total calories: {total}");
+        }
+    }
+}

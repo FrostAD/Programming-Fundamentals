7fd9c9dd9f166db579d97bf6fefc80a1c787b092
Atanas Dimov <nasko312@gmail.com>
2018-02-10 23:54:32 +0200

    Data Types and Variables - Lab

---
+++ Data Types and Variables - Lab/VolumeOfPyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeOfPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            double length;
+            length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double width;
+            width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double height;
+            height = double.Parse(Console.ReadLine());
+            double volume = 0;
+            volume = (length * width * height) / 3;
+            Console.WriteLine($"Pyramid Volume: {volume:F2}");
+        }
+    }
+}
